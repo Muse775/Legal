b7a1bfad9bd6c7eb33649cc7eb0a46e49177f384
Muse775 <gaumus.work@gmail.com>
2020-07-29 14:36:47 +0600

    final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-$('.slider__inner').slick({
+$('.slider__inner, .news__slider-inner').slick({
     nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
     prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
     infinite: false
@@ -8,6 +8,10 @@ $('.slider__inner').slick({
 });
 
 $('select').styler();
+
+$('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+});
 
 
 });
